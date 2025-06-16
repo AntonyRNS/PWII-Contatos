c4407cdedaa61b6734d43b2f064e204a98b2aaa0
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-06-16 14:40:35 +0000

    v2.1(Async await)

--- src/Pages/Contatos/Contatos.jsx
+++ src/Pages/Contatos/Contatos.jsx
@@ -7,10 +7,15 @@ function ContatosProvider({ children }) {
   const [contatoSelecionado, setContatoSelecionado] = useState(null);
 
   useEffect(() => {
-    fetch("https://run.mocky.io/v3/4cd55304-07ee-470a-93b1-d8100d1d29f2")
-      .then((res) => res.json())
-      .then((data) => setContatos(data));
+    async function buscarContatos() {
+      const res = await fetch("https://run.mocky.io/v3/4cd55304-07ee-470a-93b1-d8100d1d29f2");
+      const json = await res.json();
+      setContatos(json);
+    }
+    buscarContatos()
   }, []);
+
+
 
   return (
     <ContatosContext.Provider
@@ -20,13 +25,13 @@ function ContatosProvider({ children }) {
     </ContatosContext.Provider>
   );
 }
-export default function Contatos() {
-  return (
-    <ContatosProvider>
-      <div className="row h-100">
-        <ListaContatos />
-        <DetalhesContato />
-      </div>
-    </ContatosProvider>
-  );
-}
+  export default function Contatos() {
+    return (
+      <ContatosProvider>
+        <div className="row h-100">
+          <ListaContatos />
+          <DetalhesContato />
+        </div>
+      </ContatosProvider>
+    );
+  }
